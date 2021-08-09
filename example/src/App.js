4bb1878597d9f72025d39fdb637820f81c25caf6
Zheng Song <szhsin9@gmail.com>
2021-08-09 23:44:01 +1000

    Add GitHub link (#5)

--- example/src/App.js
+++ example/src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <a href="https://github.com/szhsin/react-transition-state"
+        title="GitHub" className="github">
+        <img src="GitHub-64.png" alt="GitHub" />
+      </a>
       <BasicExample />
     </div>
   );
