def1a45a5f09b41a58af151521c73ab6d35ba1f1
Zheng Song <41896553+szhsin@users.noreply.github.com>
2021-08-28 16:05:23 +1000

    Formatting code

--- example/src/components/BasicExample.js
+++ example/src/components/BasicExample.js
@@ -2,32 +2,35 @@ import { useState } from 'react';
 import { useTransition } from 'react-transition-state';
 
 function BasicExample() {
-    const [unmountOnExit, setUnmountOnExit] = useState(false);
-    const [state, toggle] = useTransition({
-        timeout: 750,
-        initialEntered: true,
-        preEnter: true,
-        unmountOnExit
-    });
+  const [unmountOnExit, setUnmountOnExit] = useState(false);
+  const [state, toggle] = useTransition({
+    timeout: 750,
+    initialEntered: true,
+    preEnter: true,
+    unmountOnExit
+  });
 
-    return (
-        <div className="basic-example">
-            <div className="basic-console">
-                <div className="basic-state">state: {state}</div>
-                <label>
-                    Unmount after hiding
-                    <input type="checkbox" checked={unmountOnExit}
-                        onChange={e => setUnmountOnExit(e.target.checked)} />
-                </label>
-                <button className="btn" onClick={() => toggle()}>
-                    {state === 'entering' || state === 'entered' ? 'Hide' : 'Show'}
-                </button>
-            </div>
-            {state !== 'unmounted' &&
-                <div className={`basic-transition ${state}`}>React transition state</div>
-            }
-        </div>
-    );
+  return (
+    <div className="basic-example">
+      <div className="basic-console">
+        <div className="basic-state">state: {state}</div>
+        <label>
+          Unmount after hiding
+          <input
+            type="checkbox"
+            checked={unmountOnExit}
+            onChange={(e) => setUnmountOnExit(e.target.checked)}
+          />
+        </label>
+        <button className="btn" onClick={() => toggle()}>
+          {state === 'entering' || state === 'entered' ? 'Hide' : 'Show'}
+        </button>
+      </div>
+      {state !== 'unmounted' && (
+        <div className={`basic-transition ${state}`}>React transition state</div>
+      )}
+    </div>
+  );
 }
 
 export default BasicExample;
