69160f278c0e07ba99227fe90f49b388360774db
Zheng Song <41896553+szhsin@users.noreply.github.com>
2021-09-09 21:18:29 +1000

    Test toggling state

--- src/__tests__/useTransition.test.js
+++ src/__tests__/useTransition.test.js
@@ -54,3 +54,55 @@ const renderTransitionHook = (options) => {
 
   return { result: new Result(result), render, ...rest };
 };
+
+test('should return correct value', () => {
+  const { result } = renderTransitionHook();
+
+  expect(result.length).toBe(3);
+  expect(result.state).toBe(STATES.exited);
+  expect(typeof result.toggleFn).toBe('function');
+  expect(typeof result.endTransitionFn).toBe('function');
+});
+
+test('should toggle state', () => {
+  const { result, render } = renderTransitionHook();
+  expect(render).toHaveBeenCalledTimes(1);
+
+  result.toggle();
+  expect(result.state).toBe(STATES.entering);
+
+  result.endTransition();
+  expect(result.state).toBe(STATES.entered);
+
+  result.toggle();
+  expect(result.state).toBe(STATES.exiting);
+
+  result.endTransition();
+  expect(result.state).toBe(STATES.exited);
+  expect(render).toHaveBeenCalledTimes(5);
+});
+
+test('should transition to specific state', () => {
+  const { result, render } = renderTransitionHook();
+  expect(render).toHaveBeenCalledTimes(1);
+
+  result.toggle(true);
+  expect(result.state).toBe(STATES.entering);
+  result.toggle(true);
+  expect(result.state).toBe(STATES.entering);
+  expect(render).toHaveBeenCalledTimes(2);
+
+  result.toggle(false);
+  expect(result.state).toBe(STATES.exiting);
+  result.toggle(false);
+  expect(result.state).toBe(STATES.exiting);
+  expect(render).toHaveBeenCalledTimes(3);
+
+  result.toggle(true);
+  expect(result.state).toBe(STATES.entering);
+  expect(render).toHaveBeenCalledTimes(4);
+
+  result.endTransition();
+  expect(result.state).toBe(STATES.entered);
+  expect(render).toHaveBeenCalledTimes(5);
+});
