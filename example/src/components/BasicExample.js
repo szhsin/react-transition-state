e806f084cab14054e2eec7e81fa8a782d64ee6ee
Zheng Song <41896553+szhsin@users.noreply.github.com>
2021-11-20 21:18:16 +1100

    Update examples

--- example/src/components/BasicExample.js
+++ example/src/components/BasicExample.js
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import useTransition from 'react-transition-state';
 
 function BasicExample() {
-  const [unmountOnExit, setUnmountOnExit] = useState(false);
+  const [unmountOnExit, setUnmountOnExit] = useState(true);
   const [state, toggle] = useTransition({
-    timeout: 750,
+    timeout: 500,
     initialEntered: true,
     preEnter: true,
     unmountOnExit
@@ -26,6 +26,10 @@ function BasicExample() {
         <button className="btn" onClick={() => toggle()}>
           {state === 'entering' || state === 'entered' ? 'Hide' : 'Show'}
         </button>
+        <em className="tips">
+          Tip: open the browser dev tools to verify that the following message is being moved in and
+          out of DOM.
+        </em>
       </div>
       {state !== 'unmounted' && (
         <div className={`basic-transition ${state}`}>React transition state</div>
