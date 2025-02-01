30d16cd1c66b16818a81feb6e8729cb4400bbba8
Zheng Song <szhsin9@gmail.com>
2025-02-01 18:39:19 +1100

    Use explicit file extensions for both module systems (#737)

--- dist/cjs/hooks/useTransitionState.cjs
+++ dist/cjs/hooks/useTransitionState.cjs
@@ -1,9 +1,11 @@
-import { useState, useRef, useCallback, useEffect } from 'react';
-import { getState, ENTERED, startOrEnd, getTimeout, getEndStatus, PRE_EXIT, nextTick, PRE_ENTER, EXITING, ENTERING } from './utils.js';
+'use strict';
+
+var react = require('react');
+var utils = require('./utils.cjs');
 
 const updateState = (status, setState, latestState, timeoutId, onChange) => {
   clearTimeout(timeoutId.current);
-  const state = getState(status);
+  const state = utils.getState(status);
   setState(state);
   latestState.current = state;
   onChange && onChange({
@@ -21,40 +23,40 @@ const useTransitionState = ({
   unmountOnExit,
   onStateChange: onChange
 } = {}) => {
-  const [state, setState] = useState(() => getState(initialEntered ? ENTERED : startOrEnd(mountOnEnter)));
-  const latestState = useRef(state);
-  const timeoutId = useRef();
-  const [enterTimeout, exitTimeout] = getTimeout(timeout);
-  const endTransition = useCallback(() => {
-    const status = getEndStatus(latestState.current._s, unmountOnExit);
+  const [state, setState] = react.useState(() => utils.getState(initialEntered ? utils.ENTERED : utils.startOrEnd(mountOnEnter)));
+  const latestState = react.useRef(state);
+  const timeoutId = react.useRef();
+  const [enterTimeout, exitTimeout] = utils.getTimeout(timeout);
+  const endTransition = react.useCallback(() => {
+    const status = utils.getEndStatus(latestState.current._s, unmountOnExit);
     status && updateState(status, setState, latestState, timeoutId, onChange);
   }, [onChange, unmountOnExit]);
-  const toggle = useCallback(toEnter => {
+  const toggle = react.useCallback(toEnter => {
     const transitState = status => {
       updateState(status, setState, latestState, timeoutId, onChange);
       switch (status) {
-        case ENTERING:
+        case utils.ENTERING:
           if (enterTimeout >= 0) timeoutId.current = setTimeout(endTransition, enterTimeout);
           break;
-        case EXITING:
+        case utils.EXITING:
           if (exitTimeout >= 0) timeoutId.current = setTimeout(endTransition, exitTimeout);
           break;
-        case PRE_ENTER:
-        case PRE_EXIT:
-          timeoutId.current = nextTick(transitState, status);
+        case utils.PRE_ENTER:
+        case utils.PRE_EXIT:
+          timeoutId.current = utils.nextTick(transitState, status);
           break;
       }
     };
     const enterStage = latestState.current.isEnter;
     if (typeof toEnter !== 'boolean') toEnter = !enterStage;
     if (toEnter) {
-      !enterStage && transitState(enter ? preEnter ? PRE_ENTER : ENTERING : ENTERED);
+      !enterStage && transitState(enter ? preEnter ? utils.PRE_ENTER : utils.ENTERING : utils.ENTERED);
     } else {
-      enterStage && transitState(exit ? preExit ? PRE_EXIT : EXITING : startOrEnd(unmountOnExit));
+      enterStage && transitState(exit ? preExit ? utils.PRE_EXIT : utils.EXITING : utils.startOrEnd(unmountOnExit));
     }
   }, [endTransition, onChange, enter, exit, preEnter, preExit, enterTimeout, exitTimeout, unmountOnExit]);
-  useEffect(() => () => clearTimeout(timeoutId.current), []);
+  react.useEffect(() => () => clearTimeout(timeoutId.current), []);
   return [state, toggle, endTransition];
 };
 
-export { useTransitionState };
+exports.useTransitionState = useTransitionState;
