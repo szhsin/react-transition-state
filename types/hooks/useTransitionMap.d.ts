95e1bc6420ffeeb97da4ad8e4d75156a1c740f14
Zheng Song <41896553+szhsin@users.noreply.github.com>
2026-01-01 16:57:33 +1100

    chore: clean up build

--- types/hooks/useTransitionMap.d.ts
+++
@@ -1,3 +0,0 @@
-import type { TransitionMapOptions, TransitionMapResult } from './types';
-declare const useTransitionMap: <TKey>({ allowMultiple, enter, exit, preEnter, preExit, timeout, initialEntered, mountOnEnter, unmountOnExit, onStateChange: onChange }?: TransitionMapOptions<TKey>) => TransitionMapResult<TKey>;
-export { useTransitionMap };
--- types/hooks/useTransitionState.d.ts
+++
@@ -1,2 +0,0 @@
-import type { TransitionOptions, TransitionResult } from './types';
-export declare const useTransitionState: ({ enter, exit, preEnter, preExit, timeout, initialEntered, mountOnEnter, unmountOnExit, onStateChange: onChange }?: TransitionOptions) => TransitionResult;
